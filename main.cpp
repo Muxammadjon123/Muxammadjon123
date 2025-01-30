a5d4cf529a286c44e2197d12f9611dda2ddb0d6b
m.uralov <m.uralov@newuu.uz>
2025-01-30 12:30:48 +0500

    LAB3

--- main.cpp
+++ main.cpp
@@ -1,28 +1,145 @@
-#include <iostream>;
-#include <iomanip>;
+#include <iostream>
 using namespace std;
 int main () {
-//Problem2
-    //for (int i=1; i<=5; ++i)
-        //cout<<setw(5)<<i<<endl;
+    //problem1
+    //int num1,num2,num3,num4,num5;;
+    //cout<<"Enter the numbers:";
+    //cin>>num1>>num2>>num3>>num4>>num5;
+    //int a=num1+num2+num3+num4+num5;
+    //cout<<"the sum is "<<a<<endl;
+    //return 0;
+
+
+    //Problem2
+   // int ID,year_of_study,fee;
+    //float GPA;
+    //cout<<"Enter your ID:"<<endl;
+    //cin>>ID;
+    //cout<<"Enter your gpa:"<<endl;
+    //cin>>GPA;
+    //cout<<"Enter your year of study:"<<endl;
+    //cin>>year_of_study;
+    //cout<<"Enter your study fee:"<<endl;
+    //cin>>fee;
+    //cout<<"Student ID:"<<ID<<endl;
+    //cout<<"Student GPA:"<<GPA<<endl;
+    //cout<<"Student Year of study:"<<year_of_study<<endl;
+    //cout<<"Student Study fee:"<<endl;
+    //return 0;
 
 
     //Problem3
-    //int a,b;4
-    //cin>>a>>b;
-    //Swapping values;
-    //int man=a;
-    //a=b;
-    //b=man;
-    //second way swapping
-    //cout<<"a="<<a<<" b="<<b<<endl;
+    //int celcius,fahrenheit;
+    //cout<<"Please enter the Celcius:";
+    //cin>>celcius;
+    //fahrenheit=(celcius*9/5)+32;
+    //cout<<"The temperature in Celsius: "<<celcius<<endl;
+    //cout<<"The temperature in Fahrenheit: "<<fahrenheit<<endl;
+
 
     //Problem4
-    //int Javohir;
-    //cout<<"Please enter a Javohir:";
-    //cin>>Javohir;
-    //for (int i=1; i<=Javohir; ++i)
-       //cout<<setw(5)<<i<<setw(5)<<i*i<<endl;
+    //int quantity,price;
+    //cout<<"Enter the cost:"<<endl;
+    //cin>>price;
+    //cout<<"Enter the quantity:"<<endl;
+    //cin>>quantity;
+    //int total_cost=price*quantity;
+    //cout<<"Total cost is "<<"$"<<total_cost<<endl;
+
+
+    // Problem5
+    // int age1,age2,age3,age4;
+    // cout<<"Enter the ages of memebers:";
+    // cin>>age1>>age2>>age3>>age4;
+    // float age=age1+age2+age3+age4;
+    // cout<<"Average age is "<<age/4.0;
+
+
+    //Problem6
+    // int length,width;
+    // cout<<"Enter the width:"<<endl;
+    // cin>>width;
+    // cout<<"Enter the length:"<<endl;
+    // cin>>length;
+    // int area =width*length;
+    // int perimeter=2*(width+length);
+    // cout<<"Area is: "<<area<<endl;
+    // cout<<"Perimeter is "<<perimeter<<endl;
+
+    //Problem7
+    // int dividend,divisor,quotient,remainder;
+    // cin>>dividend>>divisor;
+    // quotient=dividend/divisor;
+    // remainder=dividend%divisor;
+    // cout<<quotient<<endl;
+    // cout<<remainder<<endl;
+
+    //Problem8
+//     int UZS;
+//     float USD;
+//     cout<<"ENTER UZS:"<<endl;
+// cin>>UZS;
+//     USD=UZS/10000.0;
+//     cout<<"Amount in UZS:"<<UZS<<endl;
+//     cout<<"Exchange rate:"<<"10000.0"<<endl;
+//     cout<<"Amount in USD:"<<USD<<endl;
+
+    //Problem9
+    // int radius;
+    // cin>>radius;
+    // float surface_area=3.14*radius*2;
+    // cout<<"Surface area is:"<<surface_area<<endl;
+    // return 0;
+
+    //Problem10
+    // int height,radius;
+    // cin>>height>>radius;
+    // float volume=height*radius*3.14*radius;
+    // cout<<"Volume of cylinder with r="<<radius<<" and h="<<height<<" is "<<volume<<endl;
+
+    //Problem11
+    // int S=450;
+    // int H=2;
+    // int average_speed=S/H;
+    // float miles_hour=average_speed*0.621;
+    // cout<<"Average speed:"<<average_speed<<endl<<"Average speed in miles per hour: "<<miles_hour<<endl;
+
+    //Problem12
+    // int time;
+    // cout<<"Enter your time:"<<endl;
+    // cin>>time;
+    // int hour=time/60;
+    // int minutes=time%60;
+    // cout<<"Total minutes: "<<time<<endl<<"Total time:"<<hour<<" hours "<<minutes<<" minutes"<<endl;
+
+    //Problem13
+    // float variable_cost,price;
+    // int fixed_cost=10000.0;
+    // cout<<"Enter the price and cost:"<<endl;
+    // cin>>price>>variable_cost;
+    // float bep=fixed_cost/(price-variable_cost);
+    // cout<<"Break-even point:"<<bep<<endl;
+
+
+    //Problem14
+    // int salary,basic_salary;
+    // float tax=0.12;
+    // cin>>basic_salary;
+    // salary=basic_salary-(basic_salary*tax);
+    // cout<<"Enter basic salary: "<<basic_salary<<endl<<"Enter tax percentage: 12%"<<endl<<"Net salary:"<<salary;
+
+    //Problem15
+
+    // int original_price,discount_percentage;
+    // cin>>original_price>>discount_percentage;
+    // int discount_price=original_price*discount_percentage/100;
+    // int final_price=original_price-discount_price;
+    // cout<<"Original price:"<<original_price<<endl<<"Discount percentage:"<<discount_percentage<<endl<<"discount Amount:"<<discount_price<<endl<<"Final price:"<<final_price;
+
+//Problem16
+
+    return 0;
+
 
 
 }
