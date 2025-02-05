461845a1358ea5a613896362bdb1720405c5dea2
Muxammadjon Uralov <m.uralov@newuu.uz>
2025-02-05 17:46:30 +0500

    LAB 4

---
+++ main.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+
+    //Problem1
+    // int n;
+    // cout<<"Enter the number:";
+    // cin>>n;
+    // if (n<0)
+    //     cout<<"Negative";
+    // else
+    //     cout<<"Positive";
+
+    //Problem2
+    // int n;
+    // cout<<"Enter the number:";
+    // cin>>n;
+    // if (n%2==0)
+    //     cout<<"Output:Even";
+    // else
+    //     cout<<"Output:Odd";
+
+    //Problem3
+    // int a,b;
+    // cout<<"Enter the numbers:";
+    // cin>>a>>b;
+    // if (a<b)
+    //     cout<<"Largest number is "<<b;
+    // else
+    //     cout<<"Largest number is "<<a;
+
+    //Problem4
+    // int a,b,c;
+    // cout<<"Enter the numbers:";
+    // cin>>a>>b>>c;
+    // if ((a<c) && (b<c))
+    //     cout<<"The largest value is"<<c;
+    // else if ((a<b)&&(c<b))
+    //     cout<<"The largest value is"<<b;
+    // else if ((c<a)&&(b<a))
+    //     cout<<"The largest value is "<<a;
+
+    //Problem5
+    // int a;
+    // cout<<"Enter the year:";
+    // cin>>a;
+    // if (a%4==0)
+    //     cout<<"Output is Leap year";
+
+    //Problem6
+//     int a;
+//     cout<<"Enter the speed:";
+//     cin>>a;
+// if (a<20)
+//     cout<<"Too slow";
+//     else if (a>80)
+    // cout<<"Too fast";
+    // else
+    //     cout<<"Just right";
+
+    //Problem7
+    // int a;
+    // cout<<"Enter your score:";
+    // cin>>a;
+    // if (a>=50)
+    //     cout<<"Pass";
+    // else
+    //     cout<<"Fail";
+
+    //Problem8
+    // int a;
+    // cout<<"Enter the number:";
+    // cin>>a;
+    // if (a>0)
+    //     cout<<"Positive";
+    // else if (a<0)
+    //     cout<<"Negative";
+    // else
+    //     cout<<"Zero";
+
+    //Problem9
+    // string a;
+    // string r,y,g;
+    // cout<<"Enter the string:";
+    // cin>>a;
+    // if (a==g)
+    //     cout<<"Go";
+    // else if (a==y)
+    //     cout<<"Get ready";
+    //QUESTION 9 is not completed
+    // else if (a==r)
+    //     cout<<"Stop";
+
+    //Problem10
+    // int a;
+    // cout<<"Enter your grade:";
+    // cin>>a;
+    // if (a>=90)
+    //     cout<<"A";
+    // if ((a>=80&&a<=89))
+    //     cout<<"B";
+    // if (a>=70&&a<=79)
+    //     cout<<"C";
+    // if (a>=60&&a<=69)
+    //     cout<<"D";
+    // else if (a<=59)
+    //     cout<<"F";
+
+    //Probelm11
+    // int a,b;
+    // cout<<"Enter the numbers:";
+    // cin>>a>>b;
+    // if (a%b==0)
+    //     cout<<a<<" is divisible by "<<b;
+    // else
+    //     cout<<a<<" is not divisible by "<<b;
+
+    //Problem12
+    // int a,b,c;
+    // cout<<"Enter the angles";
+    // cin>>a>>b>>c;
+    // if (a+b+c==180)
+    //     cout<<"The triangle is valid";
+    // else
+    //     cout<<"The tringle is not valid";
+
+    //Problem13
+    //Question
+
+    //Problem14
+    // float a,b,c,d;
+    // cout<<"Enter the weights:"<<endl;
+    // cin>>a>>b;
+    // cout<<"Enter the total price:"<<endl;
+    // cin>>c>>d;
+    // float A=d/b;
+    // float B=c/a;
+    // if (A>B)
+    //     cout<<"1st shop has better price";
+    // else
+    //     cout<<"2nd shop has better price";
+
+    //Problem15
+    // int a,first_digit,last_digit;
+    // cout<<"Enter the number:";
+    // cin>>a;
+    // first_digit=a/100;
+    // last_digit=a%10;
+    // if (first_digit==last_digit) {
+    //     cout<<"The number is palindrome";
+    // }
+    // else
+    //     cout<<"The number is not palindrome";
+
+    //Problem16
+    // double x,y,distance;
+    // cout<<"Enter the coordinates:";
+    // cin>>x>>y;
+    // distance=sqrt(x*x+y*y);
+    // if (distance<=10)
+    //     cout<<"("<<x<<","<<y<<")"<<" is inside the circle";
+    // else
+    //     cout<<"("<<x<<","<<y<<")"<<" is not inside the circle";
+
+    //Problem17
+    // float a;
+    // cout<<"Enter the Grade:";
+    // cin>>a;
+    // if (a>=4.0)
+    //     cout<<"You got 80% scholarship";
+    // else if (a>=3.5)
+    //     cout<<"You got 60% scholarship";
+    // else if (a>=3.0)
+    //     cout<<"You got 50% scholarship";
+    // else
+    //     cout<<"You got no scholarhsip";
+
+    return 0;
+}
