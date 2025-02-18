c0153680cbca122f337aaa23a87e2c80f24bfced
Muxammadjon Uralov <m.uralov@newuu.uz>
2025-02-18 07:38:32 +0500

    LAB 5

--- main.cpp
+++ main.cpp
@@ -1,180 +1,275 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 int main() {
+    //  //Problem1.1
+    //  // //for (int i=1;i<=10;i++) {
+    //  // // cout<<i<<" ";
+    //  //   }
 
-    //Problem1
-    // int n;
-    // cout<<"Enter the number:";
-    // cin>>n;
-    // if (n<0)
-    //     cout<<"Negative";
-    // else
-    //     cout<<"Positive";
+    //Problem1.2
+    // int N;
+    //     cout<<"Enter the N:";
+    // cin>>N;
+    //
+    // for (int i=1;i<=N;i++) {
+    //     cout<<i<<" ";
 
-    //Problem2
-    // int n;
-    // cout<<"Enter the number:";
-    // cin>>n;
-    // if (n%2==0)
-    //     cout<<"Output:Even";
-    // else
-    //     cout<<"Output:Odd";
+    //Problem1.3
+    // int sum = 0;
+    // for (int i = 1; i <= 10; i++) {
+    //     sum += i;
+    // }
+    // cout << sum;
+
+    //Problem1.4
+    // int N,sum=0;
+    // cout<<"Enter the N:";
+    // cin>>N;
+    // for (int i = 1; i<=N; i++) {
+    //     sum += i;
+    // }
+    // cout<<sum;
+
+    //Problem1.5
+    // int sum=0;
+    // for (int i = 1; i<=10; i++) {
+    //     sum += i;
+    // }
+    // cout<<sum/10.0;
+
+    //Problem1.6
+
+    // int sum=0;
+    // float N;
+    // cin>>N;
+    // for (int i=1; i <= N;i++) {
+    //     sum += i;
+    // }
+    // cout<<sum/N;
+
+
+
+    //Problem2.1
+    // for (int i=1;i<=10;i+=2) {
+    //     cout<<i<<" ";
+    // }
+
+    //Problem2.2
+    // for (int i=2;i<=10;i+=2) {
+    //     cout<<i<<" ";
+    // }
+
+    //Problem2.3
+    // int N;
+    // cin>>N;
+    // for (int i=1;i<=N;i+=2) {
+    //     cout<<i<<" ";
+
+    //Problem2.4
+
+    // int N;
+    // cin>>N;
+    // for (int i=2;i<=N;i+=2) {
+    //     cout<<i<<" ";
+    // }
+
+    //Problem2.6
+    // int N;
+    // cin>>N;
+    // for (int i=10;i<=N;i+=10) {
+    //     cout<<i<<" ";
+
+
+    //Problem2.5
+    // for (int i=10;i<=100;i+=10) {
+    //     cout<<i<<" ";
+    // }
 
     //Problem3
-    // int a,b;
+    // int count=0;
+    // for (char string='A';string<='Z';string++) {
+    // count++;
+    //     if (string%5==0)
+    //     cout<<endl;
+    //     cout<<string<<' ';
+
+    //Problem4;
+    // int N, factorial=1;
+    // cout<<"Enter the number:";
+    // cin>>N;
+    //
+    // for (int i=N; i>=1; i--) {
+    //     factorial *= i;
+    //
+    // }
+    // cout<<factorial;
+
+
+    //schoolswtich
+    // int a,b,result=0,oper;
     // cout<<"Enter the numbers:";
     // cin>>a>>b;
-    // if (a<b)
-    //     cout<<"Largest number is "<<b;
-    // else
-    //     cout<<"Largest number is "<<a;
+    // cout<<"1:a+b\n2:a-b\n3:a*b\n4:a/b"<<endl;
+    // cout<<"Enter the operators:";
+    // cin>>oper;
+    //
+    // switch (oper) {
+    //     case 1:
+    //         result=a+b;
+    //     break;
+    //     case 2:
+    //     result=a-b;
+    //     break;
+    //     case 3:
+    //         result=a*b;
+    //     break;
+    //     case 4:
+    //     result=a/b;
+    //     break;
+    //     default:
+    //         cout<<"INVALID OPERATORS"<<endl;
+    // }
+    // cout<<result<<endl;
 
-    //Problem4
-    // int a,b,c;
-    // cout<<"Enter the numbers:";
-    // cin>>a>>b>>c;
-    // if ((a<c) && (b<c))
-    //     cout<<"The largest value is"<<c;
-    // else if ((a<b)&&(c<b))
-    //     cout<<"The largest value is"<<b;
-    // else if ((c<a)&&(b<a))
-    //     cout<<"The largest value is "<<a;
 
     //Problem5
-    // int a;
-    // cout<<"Enter the year:";
-    // cin>>a;
-    // if (a%4==0)
-    //     cout<<"Output is Leap year";
+    // int class_number,credits,total_credits=0;
+    // float grade,total_mark=0;
+    // cin>>class_number;
+    // for (int i=1;i<=class_number;i++) {
+    //     cout<<"Please enter the credits:";
+    //     cin>>credits;
+    //     total_credits+=credits;
+    //     cout<<total_credits;
+    // }
+    // for (float i=1;i<=class_number;i++) {
+    //     cout<<"Please enter the grade: ";
+    //     cin>>grade;
+    //     total_mark+=credits*grade;
+    //     cout<<total_mark;
+    // }
+    // cout<<total_mark/total_credits;
 
     //Problem6
-//     int a;
-//     cout<<"Enter the speed:";
-//     cin>>a;
-// if (a<20)
-//     cout<<"Too slow";
-//     else if (a>80)
-    // cout<<"Too fast";
-    // else
-    //     cout<<"Just right";
-
     //Problem7
-    // int a;
-    // cout<<"Enter your score:";
-    // cin>>a;
-    // if (a>=50)
-    //     cout<<"Pass";
-    // else
-    //     cout<<"Fail";
+    // int size;
+    //
+    //
+    //     cout << "Enter the size of the square: ";
+    //     cin >> size;
+    //
+    //
+    //     for (int i = 0; i < size; i++) {
+    //         for (int j = 0; j < size; j++) {
+    //             cout << "*";
+    //         }
+    //         cout << endl;
+    //     }
+    //
 
     //Problem8
-    // int a;
-    // cout<<"Enter the number:";
-    // cin>>a;
-    // if (a>0)
-    //     cout<<"Positive";
-    // else if (a<0)
-    //     cout<<"Negative";
-    // else
-    //     cout<<"Zero";
-
+        // int n;
+        // cout << "Enter the number of rows: ";
+        // cin >> n;
+        //
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= i; j++) {
+        //         cout << "*";
+        //     }
+        //     cout << endl;
+        // }
     //Problem9
-    // string a;
-    // string r,y,g;
-    // cout<<"Enter the string:";
-    // cin>>a;
-    // if (a==g)
-    //     cout<<"Go";
-    // else if (a==y)
-    //     cout<<"Get ready";
-    //QUESTION 9 is not completed
-    // else if (a==r)
-    //     cout<<"Stop";
+        // int num, i;
+        // bool isPrime = true;
+        //
+        // cout << "Enter a number: ";
+        // cin >> num;
+        //
+        // if (num <= 1) {
+        //     isPrime = false;
+        // } else {
+        //     for (i = 2; i * i <= num; i++) {
+        //         if (num % i == 0) {
+        //             isPrime = false;
+        //             break;
+        //         }
+        //     }
+        // }
+        //
+        // if (isPrime)
+        //     cout << num << " is a prime number." << endl;
+        // else
+        //     cout << num << " is not a prime number." << endl;
 
     //Problem10
-    // int a;
-    // cout<<"Enter your grade:";
-    // cin>>a;
-    // if (a>=90)
-    //     cout<<"A";
-    // if ((a>=80&&a<=89))
-    //     cout<<"B";
-    // if (a>=70&&a<=79)
-    //     cout<<"C";
-    // if (a>=60&&a<=69)
-    //     cout<<"D";
-    // else if (a<=59)
-    //     cout<<"F";
-
-    //Probelm11
-    // int a,b;
-    // cout<<"Enter the numbers:";
-    // cin>>a>>b;
-    // if (a%b==0)
-    //     cout<<a<<" is divisible by "<<b;
-    // else
-    //     cout<<a<<" is not divisible by "<<b;
+    //      int n, sum = 0;
+    // cout << "Enter the value of n: ";
+    // cin >> n;
+    //
+    // for (int i = 1; i <= n; i++) {
+    //         cout << i << " * " << i << " = " << (i * i) << endl;
+    //         sum += (i * i);
+    //     }
+    // cout << "Sum of the series: " << sum << endl;
 
     //Problem12
-    // int a,b,c;
-    // cout<<"Enter the angles";
-    // cin>>a>>b>>c;
-    // if (a+b+c==180)
-    //     cout<<"The triangle is valid";
-    // else
-    //     cout<<"The tringle is not valid";
+    // int n, reversed = 0;
+    //
+    //     cout << "Enter a number: ";
+    //     cin >> n;
+    //
+    //     cout << "Output: ";
+    //     while (n > 0) {
+    //         cout << n % 10;
+    //         n /= 10;
+    //     }
+    //     cout << endl;
 
     //Problem13
-    //Question
+// ;       int n;
+//
+//         cout << "Enter a number: ";
+//         cin >> n;
+//
+//         if (n == 0) {
+//             cout << "The depth is 0" << endl;
+//         } else {
+//             for (int i = 1; i <= n; i++) {
+//                 for (int j = 1; j <= i; j++) {
+//                     cout << i;
+//                 }
+//                 cout << endl;
+//             }
+//         }
 
     //Problem14
-    // float a,b,c,d;
-    // cout<<"Enter the weights:"<<endl;
-    // cin>>a>>b;
-    // cout<<"Enter the total price:"<<endl;
-    // cin>>c>>d;
-    // float A=d/b;
-    // float B=c/a;
-    // if (A>B)
-    //     cout<<"1st shop has better price";
-    // else
-    //     cout<<"2nd shop has better price";
+    // long long binary;
+    //     int decimal = 0, i = 0, digit;
+    //
+    //     cout << "Enter a binary number: ";
+    //     cin >> binary;
+    //
+    //
+    //     long long temp = binary;
+    //     int length = 0;
+    //
+    //     for (; temp > 0; temp /= 10) {
+    //         length++;
+    //     }
 
-    //Problem15
-    // int a,first_digit,last_digit;
-    // cout<<"Enter the number:";
-    // cin>>a;
-    // first_digit=a/100;
-    // last_digit=a%10;
-    // if (first_digit==last_digit) {
-    //     cout<<"The number is palindrome";
-    // }
-    // else
-    //     cout<<"The number is not palindrome";
 
-    //Problem16
-    // double x,y,distance;
-    // cout<<"Enter the coordinates:";
-    // cin>>x>>y;
-    // distance=sqrt(x*x+y*y);
-    // if (distance<=10)
-    //     cout<<"("<<x<<","<<y<<")"<<" is inside the circle";
-    // else
-    //     cout<<"("<<x<<","<<y<<")"<<" is not inside the circle";
+        // for (temp = binary, i = 0; i < length; temp /= 10, i++) {
+        //     digit = temp % 10;
+        //     decimal += digit * pow(2, i);
+        // }
+        //
+        // cout << "The decimal number is " << decimal << endl;
 
-    //Problem17
-    // float a;
-    // cout<<"Enter the Grade:";
-    // cin>>a;
-    // if (a>=4.0)
-    //     cout<<"You got 80% scholarship";
-    // else if (a>=3.5)
-    //     cout<<"You got 60% scholarship";
-    // else if (a>=3.0)
-    //     cout<<"You got 50% scholarship";
-    // else
-    //     cout<<"You got no scholarhsip";
 
-    return 0;
-}
+
+        return 0;
+    }
+
+
